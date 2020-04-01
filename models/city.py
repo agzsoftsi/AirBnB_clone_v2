5dd2ee0f2470665d2f20ef349db5c0c9bcfe6ade
agzsoftsi <agzsoftsi@gmail.com>
2020-04-01 04:04:57 +0000

    Update City, implement SQLalchemy, add atributes of class city and relation with state

--- models/city.py
+++ models/city.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This is the city class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel):
@@ -9,5 +11,6 @@ class City(BaseModel):
         state_id: The state id
         name: input name
     """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
